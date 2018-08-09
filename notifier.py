96f0080997f17700cc15d4ea2277bdfc08f4be7f
Neeraj M S <neerajms99b@gmail.com>
2018-08-09 09:46:53 +0200

    Logged in

--- notifier.py
+++ notifier.py
@@ -14,10 +14,12 @@ def check_for_changes():
     browser.find_element_by_partial_link_text("for students and employees").click()
     user_name = browser.find_element_by_id("username")  # Get the username element
     password = browser.find_element_by_id("password")  # Get the password element
+    button = browser.find_element_by_name("_eventId_proceed")  # Get button
     user_name.clear()  # Clear username element
     password.clear()  # Clear password element
     user_name.send_keys(studip.get("user_name"))  # Pass the value of username
     password.send_keys(studip.get("password"))  # Pass the value of password
+    button.click()
     assert "No results found." not in browser.page_source
 
 
