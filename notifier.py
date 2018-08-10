dbf215a6233c4f0ed4d4ec7bee0d839359e45f56
Neeraj M S <neerajms99b@gmail.com>
2018-08-10 07:01:44 +0200

    Click course search to

--- notifier.py
+++ notifier.py
@@ -1,6 +1,10 @@
 import urllib
 import re
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from studip_credentials import studip
 
 
@@ -12,6 +16,8 @@ def check_for_changes():
     browser = webdriver.Firefox()  # Instance of firefox webdriver is created
     browser.get(login_url)  # Open the login_url using firefox
     browser.find_element_by_partial_link_text("for students and employees").click()
+
+    # On the login page
     user_name = browser.find_element_by_id("username")  # Get the username element
     password = browser.find_element_by_id("password")  # Get the password element
     button = browser.find_element_by_name("_eventId_proceed")  # Get button
@@ -19,7 +25,14 @@ def check_for_changes():
     password.clear()  # Clear password element
     user_name.send_keys(studip.get("user_name"))  # Pass the value of username
     password.send_keys(studip.get("password"))  # Pass the value of password
-    button.click()
+    button.click()  # Click log in button
+
+    # On the user start page
+    course_search_element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "/html/body/div[2]/div[5]/ul/li[6]/a")))  # Locate the element to search for courses
+    course_search_element.click()  # Click the search button to go to search page
+
     assert "No results found." not in browser.page_source
 
 
